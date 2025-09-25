f6757ebc8490a50e44178c0e8c232b9d1878b496
ZeExotic42 <145345660+der-potatoExotic@users.noreply.github.com>
2025-09-25 15:57:11 +0200

    More hell

--- english.h
+++ english.h
@@ -2,15 +2,16 @@
 #define UTIL_H
 
 #include <Windows.h>
-
 #define IF if(
 #define THEN ) {
 #define IDFK }
 #define BUT_IF else if(
 #define ETHEN ) {
 #define IDFK }
-#define EQUALS ==
-#define EQUAL =
+#define IS_EQUALS ==
+#define IS_EQUALS_TO IS_EQUALS
+#define EQUALS =
+#define EQUALS_TO EQUALS
 #define IS =
 #define IOF ||
 #define PAND ,
@@ -18,6 +19,7 @@
 #define N_EQUALS !=
 #define ELSE else {
 #define ENDIF }
+#define END_WHEN ENDIF
 #define S_PROGRAM int main(int argc, char* argv[]){ 
 #define S_WIN int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdSHow) {
 #define END_PROGRAM return 0; }
@@ -27,18 +29,32 @@
 #define P_END );
 #define P_FEND )
 #define CALL
+#define MAKE
 #define MBOX MessageBoxA
 #define ILET int
 #define DLET double
 #define FLET float
 #define CLET char
+#define SLET const char*
 #define POSITIVE unsigned
 #define CONSTANT const
 #define POINTER *
 #define REFERENCE &
 #define REFERENCE_TO REFERENCE
 #define END ;
-
+#define NOT !
+#define CANNOT NOT
+#define CANT CANNOT
+#define SHOULD_NOT NOT
+#define SHOULDNT SHOULD_NOT
+#define STOP_WITH_CODE(x) return (x);
+#define NONE 0
+#define WHILE while(
+#define WHEN WHILE
+#define BE
+#define AS
+#define WITH
+#define GENERIC_SIZE(x) sizeof((x))
 #if __cplusplus
 #define SZERO(x) {0}
 #else
---
+++ ionglish.h
@@ -0,0 +1,17 @@
+#ifndef IONGLISH_H
+#define IONGLISH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define PRINT printf(
+#define INPUT scanf(
+#define ADD_STRING snprintf
+#define COPY_STRING strcpy
+#define COMPARE_STRING strcmp
+#define SET_MEMORY memset
+#define ALLOCATE_MEMORY malloc
+#define COPY_MEMORY memcpy
+#define CONSOLE system
+#endif
